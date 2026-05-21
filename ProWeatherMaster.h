c4c4737c366dce40a8d4a960aeefa5f6e807256e
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-21 15:14:26 +0200

    Update ProWeatherMaster.h

--- ProWeatherMaster.h
+++ ProWeatherMaster.h
@@ -65,50 +65,26 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Weather | Rain") float ThunderMaxVolume = 1.5f;
 
 	/** Sun & Light */
-	UPROPERTY(EditAnywhere, Category = "Weather | Sun")
-	float SunIntensityOfSunny = 10.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Sun")
-	float SunIntensityOfRainOrSnow = 3.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Sun")
-	float SunTemperatureOfDaytime = 5000.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Sun")
-	float SunTemperatureOfNight = 8000.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Sun") float SunIntensityOfSunny = 10.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Sun") float SunIntensityOfRainOrSnow = 3.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Sun") float SunTemperatureOfDaytime = 5000.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Sun") float SunTemperatureOfNight = 8000.0f;
 
 	/** Fog */
-	UPROPERTY(EditAnywhere, Category = "Weather | Fog")
-	float FogDensityOfSunny = 0.02f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Fog")
-	float FogDensityOfRainOrSnow = 0.05f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Fog")
-	float FogHeightFalloffOfSunny = 0.2f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Fog")
-	float FogHeightFalloffOfRainOrSnow = 0.3f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Fog") float FogDensityOfSunny = 0.02f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Fog") float FogDensityOfRainOrSnow = 0.05f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Fog") float FogHeightFalloffOfSunny = 0.2f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Fog") float FogHeightFalloffOfRainOrSnow = 0.3f;
 
 	/** Wind */
-	UPROPERTY(EditAnywhere, Category = "Weather | Wind")
-	float WindSpeedOfSnowOrRain = 6.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Wind")
-	float WindSpeedOfSunny = 3.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Wind")
-	float WindWeightOfSnowOrRain = 8.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | Wind")
-	float WindWeightOfSunny = 2.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Wind") float WindSpeedOfSnowOrRain = 6.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Wind") float WindSpeedOfSunny = 3.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Wind") float WindWeightOfSnowOrRain = 8.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | Wind") float WindWeightOfSunny = 2.0f;
 
 	/** Skylight */
-	UPROPERTY(EditAnywhere, Category = "Weather | SkyLight")
-	float SkyLightIntensityOfDay = 1.2f;
-
-	UPROPERTY(EditAnywhere, Category = "Weather | SkyLight")
-	float SkyLightIntensityOfNight = 1.0f;
+	UPROPERTY(EditAnywhere, Category = "Weather | SkyLight") float SkyLightIntensityOfDay = 1.2f;
+	UPROPERTY(EditAnywhere, Category = "Weather | SkyLight") float SkyLightIntensityOfNight = 1.0f;
 
 	/** Core Functions */
 	void SetupDefaults();
@@ -122,54 +98,27 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	/** Componentes principales */
-	UPROPERTY(EditAnywhere)
-	UDirectionalLightComponent* DirectionalLight;
-
-	UPROPERTY(EditAnywhere)
-	USkyLightComponent* SkyLight;
-
-	UPROPERTY(EditAnywhere)
-	USkyAtmosphereComponent* SkyAtmosphere;
-
-	UPROPERTY(EditAnywhere)
-	UPostProcessComponent* PostProcessComp;
-
-	UPROPERTY(EditAnywhere)
-	UVolumetricCloudComponent* VolumetricCloud;
-
-	UPROPERTY(EditAnywhere)
-	UExponentialHeightFogComponent* ExponentialHeightFog;
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* SkySphere;
+	UPROPERTY(EditAnywhere) UDirectionalLightComponent* DirectionalLight;
+	UPROPERTY(EditAnywhere) USkyLightComponent* SkyLight;
+	UPROPERTY(EditAnywhere) USkyAtmosphereComponent* SkyAtmosphere;
+	UPROPERTY(EditAnywhere) UPostProcessComponent* PostProcessComp;
+	UPROPERTY(EditAnywhere) UVolumetricCloudComponent* VolumetricCloud;
+	UPROPERTY(EditAnywhere) UExponentialHeightFogComponent* ExponentialHeightFog;
+	UPROPERTY(EditAnywhere) UStaticMeshComponent* SkySphere;
 
 	/** Niagara FX */
-	UPROPERTY(EditAnywhere)
-	UNiagaraComponent* RainFX;
-
-	UPROPERTY(EditAnywhere)
-	UNiagaraComponent* SnowFX;
+	UPROPERTY(EditAnywhere) UNiagaraComponent* RainFX;
+	UPROPERTY(EditAnywhere) UNiagaraComponent* SnowFX;
 
 	/** Audio */
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* RainAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* RainAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* ThunderAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* SnowAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* BirdsAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* WindAudio;
+	UPROPERTY(EditAnywhere) UAudioComponent* CicadaAudio;
 
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* ThunderAudio;
-
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* SnowAudio;
-
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* BirdsAudio;
-
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* WindAudio;
-
-	UPROPERTY(EditAnywhere)
-	UAudioComponent* CicadaAudio;
-
-	/** Sound cues (Asignados en el .cpp o editor) */
+	/** Sound cues (Asignados en el .cpp y/o editor) */
 	USoundCue* RainSound;
 	USoundCue* ThunderSound;
 	USoundCue* SnowSound;
@@ -182,68 +131,29 @@ public:
 	UMaterialParameterCollection* MPC_TreeWind;
 
 	/** Curves */
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* IsNightCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* SunAngleCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* SnowOrRainFallingCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* RainFallingCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* SnowFallingCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* GetWetCurve;
-
-	UPROPERTY(EditAnywhere)
-	UCurveFloat* FrozenCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* IsNightCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* SunAngleCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* SnowOrRainFallingCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* RainFallingCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* SnowFallingCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* GetWetCurve;
+	UPROPERTY(EditAnywhere) UCurveFloat* FrozenCurve;
 
 	/** Timelines */
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* IsNightTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* SunTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* SnowOrRainFallingTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* RainFallingTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* SnowFallingTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* GetWetTimeLine;
-
-	UPROPERTY(EditAnywhere)
-	UTimelineComponent* FrozenTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* IsNightTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* SunTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* SnowOrRainFallingTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* RainFallingTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* SnowFallingTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* GetWetTimeLine;
+	UPROPERTY(EditAnywhere) UTimelineComponent* FrozenTimeLine;
 
 	/** Timeline callbacks */
-	UFUNCTION()
-	void UpdateIsNightTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateSunTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateSnowOrRainFallingTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateRainFallingTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateSnowFallingTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateGetWetTimeLine(float Output);
-
-	UFUNCTION()
-	void UpdateFrozenTimeLine(float Output);
+	UFUNCTION() void UpdateIsNightTimeLine(float Output);
+	UFUNCTION() void UpdateSunTimeLine(float Output);
+	UFUNCTION() void UpdateSnowOrRainFallingTimeLine(float Output);
+	UFUNCTION() void UpdateRainFallingTimeLine(float Output);
+	UFUNCTION() void UpdateSnowFallingTimeLine(float Output);
+	UFUNCTION() void UpdateGetWetTimeLine(float Output);
+	UFUNCTION() void UpdateFrozenTimeLine(float Output);
 };
